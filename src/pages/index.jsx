7dc81bc7a59cfb353ec45c5442905aadc7e106c9
swe-jaeyoungpark <swe.jaeyoungpark@navercorp.com>
2019-03-12 22:33:38 +0900

    [Issue #2] Add open graph tags

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Post from '../components/Post';
 import Sidebar from '../components/Sidebar';
+import profilePic from '../pages/photo.jpg';
 
 class IndexRoute extends React.Component {
   render() {
     const items = [];
-    const { title, subtitle } = this.props.data.site.siteMetadata;
+    const { title, subtitle, siteUrl } = this.props.data.site.siteMetadata;
     const posts = this.props.data.allMarkdownRemark.edges;
     posts.forEach((post) => {
       items.push(<Post data={post} key={post.node.fields.slug} />);
@@ -18,6 +19,11 @@ class IndexRoute extends React.Component {
           <title>{title}</title>
           <meta name="description" content={subtitle} />
           <meta name="naver-site-verification" content="cd14eb7aa77c570360a07270cab55fbabb276d18" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={subtitle} />
+          <meta property="og:image" content={profilePic} />
+          <meta property="og:url" content={siteUrl} />
         </Helmet>
         <Sidebar siteMetadata={this.props.data.site.siteMetadata} />
         <div className="content">
