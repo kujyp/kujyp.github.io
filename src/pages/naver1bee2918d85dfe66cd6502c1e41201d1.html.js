43d9a538e6537703cbdae84800cab0ca3f0c1b85
swe-jaeyoungpark <swe.jaeyoungpark@navercorp.com>
2019-03-13 01:40:43 +0900

    Add naver site verfication html file

---
+++ src/pages/naver1bee2918d85dfe66cd6502c1e41201d1.html.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NaverVerification extends React.Component {
+  render() {
+    return (
+      <div>
+        naver-site-verification: naver1bee2918d85dfe66cd6502c1e41201d1.html
+      </div>
+    );
+  }
+}
+
+export default NaverVerification;
