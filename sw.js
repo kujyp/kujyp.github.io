2a359ece7ca8d563688523c3f6725fe6178d44fb
kujyp <swe.jaeyoungpark@gmail.com>
2021-04-21 00:00:12 +0000

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-2a98cc2fd86302711cfd.js","53a24c41f27bc0bc9544aaa221e13719"],["/commons-1cfc51756e40a3763f91.js","0c84fc052c3732449ad81840162e6a37"],["/index.html","dc42ecb8560acd8aac2e80632ab04520"],["/offline-plugin-app-shell-fallback/index.html","e72bdc8f4d6125cd0c223a9e0b6bdcb0"]];
+var precacheConfig = [["/app-c2b978785e76ab360179.js","ac2a13eff0a54605acb0d92ac9aec334"],["/commons-1cfc51756e40a3763f91.js","0c84fc052c3732449ad81840162e6a37"],["/index.html","977591424b8311f322da16e42f6e4ac4"],["/offline-plugin-app-shell-fallback/index.html","67e27c5677c1facfc3acb40edf922163"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
